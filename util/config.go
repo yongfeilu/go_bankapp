7a210ae3c0f1965579d4e043b0bc7b8e39a3c979
Yongfei Lu <yflecon@gmail.com>
2022-05-19 04:52:40 -0700

    DB part

---
+++ util/config.go
@@ -0,0 +1,16 @@
+package util
+
+import "time"
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variable.
+type Config struct {
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE"`
+	MigrationURL         string        `mapstructure:"MIGRATION_URL"`
+	HTTPServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
+	GRPCServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+}
